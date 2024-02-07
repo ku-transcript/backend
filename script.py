847bb31fa129f897eaf878ddb194dcc64380e8ed
Glitch (gilded-filament) <none>
2024-02-07 21:15:13 +0000

    🐣⚽️ Checkpoint ./script.py:55729241/332:64602179/34

--- script.py
+++ script.py
@@ -1,11 +1,12 @@
 from PyPDF2 import PdfReader 
-# import PyPDF2
 import re
 import urllib.request
 import io
 
 def getStudentCoursesFromPDF(file):
   reader = PdfReader(file)
+  
+  data = {}
   courses = []
 
   # getting a specific page from the pdf file 
@@ -15,8 +16,13 @@ def getStudentCoursesFromPDF(file):
       text = page.extract_text() 
 
       matches = re.findall(r"([A-D][+]?|[FPNW]) (\d{1}) ([\w ]+) (\d{8})", text, re.MULTILINE)
-      name = re.findall(r"(Miss|Mr\.) ([\w ]+ [\w ]+)", text, re.MULTILINE)[0]
+      title, name = re.findall(r"(Miss|Mr\.) ([\w ]+ [\w ]+)", text, re.MULTILINE)[0]
       major = re.findall(r"(Field Of Study) ([\w ]+) (Miss|Mr\.)", text, re.MULTILINE)[0][1]
+      # [(_, _, _)]
+      
+      data['student_title'] = title
+      data['student_name'] = name
+      data['student_major'] = major
       
       for match in matches:
           student_grade, course_credit, course_name, course_id = match
@@ -29,7 +35,9 @@ def getStudentCoursesFromPDF(file):
 
           print(student_grade, course_credit, course_name, course_id)
   
-  return courses
+  data['enrolled_courses'] = courses
+  
+  return data
   
 # # Read PDF file remotely
 # URL = 'https://cdn.glitch.global/b47f022b-8699-4b00-bf3c-d2513ddf3842/STD_GRADE_REPORT_6410406860.pdf?v=1707336428329'
