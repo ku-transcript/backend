4e4a877815c9ff8e9f06d0d06eba30c2908d662e
Glitch (gilded-filament) <none>
2024-02-07 20:28:12 +0000

    🌍🎉 Checkpoint ./script.py:55729241/624 ./server.py:55729241/1744

--- script.py
+++ script.py
@@ -1,8 +1,18 @@
 from PyPDF2 import PdfReader 
+# import PyPDF2
 import re
+import urllib.request
+import io
+
+
+# Read PDF file remotely
+URL = 'https://cdn.glitch.global/b47f022b-8699-4b00-bf3c-d2513ddf3842/STD_GRADE_REPORT_6410406860.pdf?v=1707336428329'
+req = urllib.request.Request(URL, headers={'User-Agent' : "Magic Browser"})
+remote_file = urllib.request.urlopen(req).read()
+remote_file_bytes = io.BytesIO(remote_file)
   
 # creating a pdf reader object 
-reader = PdfReader('/public/STD_GRADE_REPORT_6410406860.pdf') 
+reader = PdfReader(remote_file_bytes)
   
 # printing number of pages in pdf file 
 print(len(reader.pages)) 
--- server.py
+++ server.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-#       _                              
-#      | |                             
-#    __| |_ __ ___  __ _ _ __ ___  ___ 
-#   / _` | '__/ _ \/ _` | '_ ` _ \/ __|
-#  | (_| | | |  __/ (_| | | | | | \__ \
-#   \__,_|_|  \___|\__,_|_| |_| |_|___/ .
-#
-# A 'Fog Creek'–inspired demo by Kenneth Reitz™
 
 import os
 from flask import Flask, request, render_template, jsonify
@@ -23,41 +13,14 @@ app.secret = os.environ.get('SECRET')
 # Dream database. Store dreams in memory for now. 
 DREAMS = ['Python. Python, everywhere.']
 
-
-@app.after_request
-def apply_kr_hello(response):
-    """Adds some headers to all responses."""
-  
-    # Made by Kenneth Reitz. 
-    if 'MADE_BY' in os.environ:
-        response.headers["X-Was-Here"] = os.environ.get('MADE_BY')
-    
-    # Powered by Flask. 
-    response.headers["X-Powered-By"] = os.environ.get('POWERED_BY')
-    return response
-
-
 @app.route('/')
 def homepage():
     """Displays the homepage."""
     return render_template('index.html')
+  
+# @app.route('/dreams', methods=['GET', 'POST'])
+# def helloworld():
 
-@app.route('/lol')
-def rlol():
-    return jsonify(test.lol())
-  
-@app.route('/dreams', methods=['GET', 'POST'])
-def dreams():
-    """Simple API endpoint for dreams. 
-    In memory, ephemeral, like real dreams.
-    """
-  
-    # Add a dream to the in-memory database, if given. 
-    if 'dream' in request.args:
-        DREAMS.append(request.args['dream'])
-    
-    # Return the list of remembered dreams. 
-    return jsonify(DREAMS)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
