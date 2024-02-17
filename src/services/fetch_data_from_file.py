5b12cf10ef15c2569f725c1eef817b605e5dc0db
Glitch (gilded-filament) <none>
2024-02-17 16:53:25 +0000

    🎺🎓 Checkpoint ./src/services/fetch_data_from_file.py:55729241/279

--- src/services/fetch_data_from_file.py
+++ src/services/fetch_data_from_file.py
@@ -2,19 +2,25 @@ import json
 
 class FileData:
   
+  '''
+  Read data from json file
+  
+  @return list of courses
+  '''
   def fetch(self):
     file1 = open("data/ku_ge.json")
     file2 = open("data/ku_cs60.json")
     
-    data1 = json.load(file1)
-    data2 = json.load(file2)
+    data1 = json.load(file1) # list of gened courses
+    data2 = json.load(file2) # list of cs courses
     
-    
-    # print("Return data = ", data)
-    
-    return data
+    data1.extend(data2) # merge two lists
+        
+    return data1
   
 
 f = FileData()
 
-f.fetch()
+data = f.fetch()
+
+print(data)
