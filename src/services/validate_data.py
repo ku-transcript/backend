1f1f28b6d3d50f4219ea6eb3f42f9632b1c254fd
Glitch (gilded-filament) <none>
2024-02-18 17:09:07 +0000

    💨🖖 Checkpoint ./src/services/validate_data.py:55729241/418 ./src/services/check_graduation.py:55729241/21749

--- src/services/validate_data.py
+++ src/services/validate_data.py
@@ -1,3 +1,11 @@
+from calculate_total_credit import calculate
+from fetch_data_from_file import FileData
+
 def validate(student_data, validator):
+  f = FileData()
+  courses = f.fetch()
+  total_credit = calculate(student_data, courses)
   
-  return validator.validate()
+  return validator.validate(student_data, total_credit)
+
+
