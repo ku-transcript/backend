3c852c7ced47c482c2a80fef72910aa4ba9d81c2
Glitch (gilded-filament) <none>
2024-02-19 17:07:23 +0000

    👏📒 Checkpoint ./server.py:55729241/863

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import os
 from flask import Flask, request, render_template, jsonify
+# from flask_api import status
 from script import get_student_data
 import sys
 sys.path.append("src/services")
@@ -16,35 +17,35 @@ app = Flask(__name__, static_folder='public', template_folder='views')
 app.secret = os.environ.get('SECRET')
 
 def allowed_file(filename):
-    return '.' in filename and \
+  ALLOWED_EXTENSION = { '.pdf' }
+  return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def homepage():
-    """Displays the homepage."""
-    return render_template('index.html')
+  """Displays the homepage."""
+  return render_template('index.html')
 
 @app.route('/api/upload', methods=['POST'])
 def upload():
-  ALLOWED_EXTENSION = { '.pdf' }
   
   if request.method == 'POST':   
     
-        # TODO: check file type is PDF
-        
-        if 'file' not in request.files:
-          return
-        
-        f = request.files['file'] 
-        
-        if f and allowed_file(f)
-        
-        student_data = get_student_data(f)
-        student_data.update({
-          "total_credit_per_category": calculate(student_data["enrolled_courses"], DataSourceFile())
-        })
-        
-        return jsonify(check_graduation(student_data))
+    # Check file type is PDF
+    if 'file' not in request.files:
+      return "No file uploaded", 400
+
+    f = request.files['file'] 
+
+    if not allowed_file(f):
+      return "File type not allowed only support pdf", 400
+
+    student_data = get_student_data(f)
+    student_data.update({
+      "total_credit_per_category": calculate(student_data["enrolled_courses"], DataSourceFile())
+    })
+
+    return jsonify(check_graduation(student_data))
   
 if __name__ == '__main__':
-    app.run(debug = True)
+  app.run(debug = True)
