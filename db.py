37d9b75a359c7560dcdd25b44c818a251bb48d65
Qu1etboy <nonzagreanthai@gmail.com>
2024-02-25 16:09:07 +0000

    🍟🏠 Checkpoint ./db.py:55729241/139

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import sqlite3
 import os
 import click
 from flask import current_app, g
+from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
@@ -28,10 +29,12 @@ def init_db():
 
 
 @click.command('init-db')
+@with_appcontext
 def init_db_command():
     init_db()
     click.echo('Initialized the database.')
     
 def init_app(app):
     app.teardown_appcontext(close_db)
-    # app.cli.add_command(init_db_command)
+    app.cli.add_command(init_db_command)
+    print("Done")
