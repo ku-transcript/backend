4b5557d77633fcc04a7f3f03b5290f8d69bec467
Qu1etboy <nonzagreanthai@gmail.com>
2024-03-24 00:41:40 +0700

    feat: check graduation

---
+++ internal/pkg/calculator/credit.go
@@ -0,0 +1,40 @@
+package calculator
+
+import (
+	"ku-transcript/internal/pkg/utils/data"
+	"ku-transcript/internal/pkg/utils/parser"
+)
+
+func CalculateTotalCredits(enrolled_courses []parser.Course, required_credits map[string]int) (map[string]int, int) {
+
+	courses := data.ReadCourses()
+
+	totalCredits := make(map[string]int)
+	total := 0
+
+	for _, enrolledCourse := range enrolled_courses {
+		for _, c := range courses {
+			if enrolledCourse.CourseID != c.CourseID || enrolledCourse.StudentGrade == "W" || enrolledCourse.StudentGrade == "F" || enrolledCourse.StudentGrade == "P" {
+				continue
+			}
+
+			if _, ok := totalCredits[c.CourseCategory]; !ok && c.CourseCategory != "" {
+				totalCredits[c.CourseCategory] = 0
+			}
+			if _, ok := totalCredits[c.CourseFaculty]; !ok && c.CourseFaculty != "" {
+				totalCredits[c.CourseFaculty] = 0
+			}
+
+			if c.CourseCategory != "" && totalCredits[c.CourseCategory] < required_credits[c.CourseCategory] {
+				totalCredits[c.CourseCategory] += c.CourseCredit
+			} else if c.CourseFaculty != "" && totalCredits[c.CourseFaculty] < required_credits[c.CourseFaculty] {
+				totalCredits[c.CourseFaculty] += c.CourseCredit
+			} else {
+				totalCredits["วิชาเสรี"] += c.CourseCredit
+			}
+			total += c.CourseCredit
+		}
+	}
+
+	return totalCredits, total
+}
