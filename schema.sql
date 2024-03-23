61ccdb468115c1f31a26e80713a64dd00647f603
Qu1etboy <nonzagreanthai@gmail.com>
2024-03-23 16:02:32 +0700

    feat: extract and parse text from pdf

--- schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS courses;
-
-CREATE TABLE courses (
-    course_id CHAR(8) PRIMARY KEY, 
-    course_name VARCHAR(255), 
-    course_credit INT, 
-    course_category VARCHAR(255), 
-    course_faculty VARCHAR(255)
-);
