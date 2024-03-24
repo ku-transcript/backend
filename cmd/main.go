5ec5d890236e29505fce1dd82ab1fc3ae65addff
Qu1etboy <nonzagreanthai@gmail.com>
2024-03-24 16:21:21 +0700

    feat: allow cors

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"ku-transcript/internal/app/controllers"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/swagger"
 
 	_ "ku-transcript/docs"
@@ -13,6 +14,7 @@ func main() {
 
 	app := fiber.New()
 
+	app.Use(cors.New())
 	app.Get("/swagger/*", swagger.HandlerDefault)
 
 	controllers.Init(app)
